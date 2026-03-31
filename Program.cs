b311e449a418812d1e4b9a27403332bc903e2eac
Tin Nguyen <tinnguyen8426@gmail.com>
2026-03-31 20:19:46 +0700

    fix: allow any origin for CORS to support Render deployment

--- Program.cs
+++ Program.cs
@@ -19,10 +19,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactClient", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://frontend:3000")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 builder.Services.AddDistributedMemoryCache();
